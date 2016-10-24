b6d0d7999eb7792a01670336a27f26b953a903e4
Zachary Browne <browne.zachary@gmail.com>
2016-10-23 17:16:33 -0700

    trying to create two dictionaries: one stable dictionary with continuous updating values and one "temporary" dictionary that updates the stable one. It's not working :(

--- GTFS/ViewController.swift
+++ GTFS/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController, XMLParserDelegate, CLLocationManagerDele
     var xmlParser: XMLParser!
     let locationManager = CLLocationManager()
     
-    var vehicles = [Vehicle]()
+    var currentVehicles = [String: Vehicle]()
+    var updatedVehicles = [String: Vehicle]()
     var elements = [String]()
     var lastUpdated = String()
     let regionRadius: CLLocationDistance = 20000
@@ -39,9 +40,11 @@ class ViewController: UIViewController, XMLParserDelegate, CLLocationManagerDele
     
     // zooms to user's current location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("did update location")
         if let userLocation = locations.last {
             let center = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
             mapView.setCenter(center, zoomLevel: 15, animated: true)
+            print("setting center")
         }
     }
     
@@ -49,7 +52,6 @@ class ViewController: UIViewController, XMLParserDelegate, CLLocationManagerDele
     }
     
     // reads nextbus xml data and starts the xml parser delegate methods
-    // TODO - Make addVehiclesToMap work!!!!!!!!!!
     
     func refreshData() {
         let urlString = NSURL(string: "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni")
@@ -61,10 +63,16 @@ class ViewController: UIViewController, XMLParserDelegate, CLLocationManagerDele
             self.xmlParser.delegate = self
             self.xmlParser.parse()
             print (String(self.elements.count) + " elements parsed")
-            print (String(self.vehicles.count) + " vehicle locations obtained")
+            print (String(self.updatedVehicles.count) + " vehicle locations obtained")
+            print (String(self.currentVehicles.count) + " vehicles in the current vehicle list")
             print ("last updated at " + (self.lastUpdated))
-            // self.addVehiclesToMap()
-            self.vehicles.removeAll()
+            let cv = self.currentVehicles
+            DispatchQueue.main.async {
+                print ("adding vehicles to map on main...")
+                self.addVehiclesToMap(v: Array(cv.values))
+                print ("... done adding vehicles to map on main")
+            }
+            self.updatedVehicles.removeAll()
             self.elements.removeAll()
             NSLog ("task completed")
             }.resume()
@@ -122,7 +130,8 @@ class ViewController: UIViewController, XMLParserDelegate, CLLocationManagerDele
             
             let vehicle = Vehicle(title: id, routeTag: routeTag, dirTag: dirTag, lat: lat, lon: lon, secsSinceReport: secsSinceReport, predictable: predictable, heading: heading, speedKmHr: speedKmHr, leadingVehicleId: leadingVehicleId)
             
-            vehicles.append(vehicle)
+            updatedVehicles[id] = vehicle
+            updateCurrentVehicles()
         }
         
         if elementName == "lastTime" {
@@ -131,6 +140,38 @@ class ViewController: UIViewController, XMLParserDelegate, CLLocationManagerDele
                 lastUpdated = convertToDeviceTime(timestamp: unixTime)
             } else {
                 print ("time of last update not available")
+            }
+        }
+    }
+    
+    // currently not doing anything!!! 
+    func updateCurrentVehicles() {
+        // iterate through the most recent GTFS vehicle feed, update previous vehicle info and add new vehicles if they don't exist
+        for updatedid in updatedVehicles.keys {
+            for currentid in currentVehicles.keys {
+                if updatedid == currentid {
+                    // if updated id matches a current id, set the current id's value to that of the updated id
+                    currentVehicles[currentid] = updatedVehicles[updatedid]
+                }
+                else {
+                    // if updated id matches none of the current id's, add it to the current id dictionary
+                    currentVehicles[updatedid] = updatedVehicles[updatedid]
+                    print ("vehicle added to current vehicle list")
+                }
+            }
+        }
+        // iterate through the current vehicle list and remove any vehicles that are not present in the most recent GTFS feed
+        for currentid in currentVehicles.keys {
+            for updatedid in updatedVehicles.keys {
+                if currentid == updatedid {
+                    // break out and move to the next current id
+                    break
+                }
+                else {
+                    // if current id matches none of the updated id's, remove it from the dictionary
+                    currentVehicles[currentid] = nil
+                    print ("vehicle removed from current vehicle list")
+                }
             }
         }
     }
@@ -153,34 +194,40 @@ class ViewController: UIViewController, XMLParserDelegate, CLLocationManagerDele
     
     // mapping functions
     
-    func addVehiclesToMap() {
-        for vehicle in vehicles {
+    func addVehiclesToMap(v: [Vehicle]) {
+        for vehicle in v {
             mapView.addAnnotation(vehicle)
         }
     }
-    
-    
 }
 
 // extention for mapview delegates
 
 extension ViewController: MGLMapViewDelegate {
     
-    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+/*    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
         
-        // Use the annotation’s longitude value (as a string) as the reuse identifier for its view.
+        // we only want vehicle annotations
+        guard annotation is Vehicle else {
+            return nil
+        }
+        
         let reuseIdentifier = "\(annotation.coordinate.longitude)"
-        
-        // For better performance, always try to reuse existing annotations.
+
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
         
-        // If there’s no reusable annotation view available, initialize a new one.
         if annotationView == nil {
-            annotationView = MGLAnnotationView(reuseIdentifier: reuseIdentifier)
+            annotationView = CustomAnnotationView(reuseIdentifier: reuseIdentifier)
+            annotationView!.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            
+            // Set the annotation view’s background color to a value determined by its longitude.
+            let hue = CGFloat(annotation.coordinate.longitude) / 100
+            annotationView!.backgroundColor = UIColor(hue: hue, saturation: 0.5, brightness: 1, alpha: 1)
         }
+        
         return annotationView
-    }
-    
+    } */
+
     // Allow callout view to appear when an annotation is tapped.
     func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         return true
@@ -203,6 +250,20 @@ extension ViewController: MGLMapViewDelegate {
         let ac = UIAlertController(title: routeTag, message: secsSinceReport, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+}
+
+class CustomAnnotationView: MGLAnnotationView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Force the annotation view to maintain a constant size when the map is tilted.
+        scalesWithViewingDistance = false
+        
+        // Use CALayer’s corner radius to turn this view into a circle.
+        layer.cornerRadius = frame.width / 2
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
     }
 }
 
